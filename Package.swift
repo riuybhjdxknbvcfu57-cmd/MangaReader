1c8f49544d0989bd6345a4c717d57d4e2ceaa58a
riuybhjdxknbvcfu57-cmd <riuybhjdxknbvcfu57@gmail.com>
2025-12-29 10:16:18 +0000

    Disable strict linting and temporarily remove MangaDexLib dependency

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
         )
     ],
     dependencies: [
-        .package(url: "https://github.com/JRomainG/MangaDexLib", branch: "main"),
+        // .package(url: "https://github.com/JRomainG/MangaDexLib", branch: "dev"),
         .package(url: "https://github.com/Alamofire/Alamofire", from: "5.8.0"),
         .package(url: "https://github.com/onevcat/Kingfisher", from: "7.10.0")
     ],
@@ -21,7 +21,7 @@ let package = Package(
         .target(
             name: "MangaReader",
             dependencies: [
-                "MangaDexLib",
+                // "MangaDexLib",
                 "Alamofire",
                 "Kingfisher"
             ]
